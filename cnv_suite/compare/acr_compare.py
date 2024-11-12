436d4404728bd87b99b9a1923c8c315ba0e8ece9
Ron Solan <rsolan@broadinstitute.org>
2024-11-12 14:40:41 -0500

    Added purity comparisons.

--- cnv_suite/compare/acr_compare.py
+++ cnv_suite/compare/acr_compare.py
@@ -2,6 +2,9 @@ from collections import defaultdict
 from dataclasses import dataclass
 import glob
 import os
+import re
+from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
 import numpy as np
 import numpy.typing as npt
 import pandas as pd
@@ -16,12 +19,15 @@ from math import log
 import sys
 from scipy.optimize import minimize
 from utils import BASE_PATH, PathLike
+import random
 
 STAT_COLUMNS = ["mu.minor", "sigma.minor", "mu.major", "sigma.major"]
 
 
 def acr_compare(
-    file_1: pd.DataFrame | PathLike | None = None, file_2: pd.DataFrame | PathLike | None = None, fit_params=False
+    file_1: pd.DataFrame | PathLike | None = None,
+    file_2: pd.DataFrame | PathLike | None = None,
+    fit_params=False,
 ):
     """
     Compare the allelic copy ratio segments between the two seg files.
@@ -83,18 +89,31 @@ def acr_compare(
 
     if fit_params:
         bins["mu.major_1"] = bins.apply(
-            lambda x: optimal_scale_factor * (optimal_purity * (x["mu.major_1"] - 1) + 1), axis=1
+            lambda x: optimal_scale_factor
+            * (optimal_purity * (x["mu.major_1"] - 1) + 1),
+            axis=1,
         )
         bins["mu.minor_1"] = bins.apply(
-            lambda x: optimal_scale_factor * (optimal_purity * (x["mu.minor_1"] - 1) + 1), axis=1
+            lambda x: optimal_scale_factor
+            * (optimal_purity * (x["mu.minor_1"] - 1) + 1),
+            axis=1,
         )
         bins["sigma.major_1"] *= optimal_scale_factor
         bins["sigma.minor_1"] *= optimal_scale_factor
 
-    non_overlap_length = int(bins["length_1_unique"].sum()) + int(bins["length_2_unique"].sum())
+    non_overlap_length = int(bins["length_1_unique"].sum()) + int(
+        bins["length_2_unique"].sum()
+    )
     overlap_length = int(bins["length_overlap"].sum())
 
-    return overlap_score, optimal_scale_factor, optimal_purity, non_overlap_length, overlap_length, bins
+    return (
+        overlap_score,
+        optimal_scale_factor,
+        optimal_purity,
+        non_overlap_length,
+        overlap_length,
+        bins,
+    )
 
 
 def overlap_min_helper(params, bins):
@@ -118,26 +137,42 @@ def get_avg_overlap(bins, params=None):
     # can I assume allele marker (major vs. minor) is same for both inputs? todo
     major_overlap = bins.apply(
         lambda x: calc_overlap(
-            x["mu.major_1"], x["sigma.major_1"], x["mu.major_2"], x["sigma.major_2"], x["unique"], params=params
+            x["mu.major_1"],
+            x["sigma.major_1"],
+            x["mu.major_2"],
+            x["sigma.major_2"],
+            x["unique"],
+            params=params,
         ),
         axis=1,
     )
     minor_overlap = bins.apply(
         lambda x: calc_overlap(
-            x["mu.minor_1"], x["sigma.minor_1"], x["mu.minor_2"], x["sigma.minor_2"], x["unique"], params=params
+            x["mu.minor_1"],
+            x["sigma.minor_1"],
+            x["mu.minor_2"],
+            x["sigma.minor_2"],
+            x["unique"],
+            params=params,
         ),
         axis=1,
     )
 
     # get average overlap score
     overlap_score = np.average(
-        np.concatenate((major_overlap, minor_overlap)), weights=np.concatenate([bins["length"], bins["length"]])
+        np.concatenate((major_overlap, minor_overlap)),
+        weights=np.concatenate([bins["length"], bins["length"]]),
     )
     return overlap_score, major_overlap, minor_overlap
 
 
 def calc_overlap(
-    mu1: float, sigma1: float, mu2: float, sigma2: float, unique: bool, params: np.ndarray | None = np.array([1.0, 1.0])
+    mu1: float,
+    sigma1: float,
+    mu2: float,
+    sigma2: float,
+    unique: bool,
+    params: np.ndarray | None = np.array([1.0, 1.0]),
 ):
     """
     Calculate the overlap between two normal distributions, defined by given statistics.
@@ -185,13 +220,19 @@ def calc_overlap(
     if len(x_intersect) == 1:  # sigma1 == sigma2
         area = NormalDist(mu1, sigma1).cdf(x_intersect[0])
         if area < 0.5:
-            return area * 2  # doubled -> pdf1_area = pdf2_area when sigma1 = sigma2
+            return (
+                area * 2
+            )  # doubled -> pdf1_area = pdf2_area when sigma1 = sigma2
         else:  # take other side of cdf
             return (1 - area) * 2
 
     # calculate overlap cdf
-    mid_section1 = NormalDist(mu1, sigma1).cdf(max(x_intersect)) - NormalDist(mu1, sigma1).cdf(min(x_intersect))
-    mid_section2 = NormalDist(mu2, sigma2).cdf(max(x_intersect)) - NormalDist(mu2, sigma2).cdf(min(x_intersect))
+    mid_section1 = NormalDist(mu1, sigma1).cdf(max(x_intersect)) - NormalDist(
+        mu1, sigma1
+    ).cdf(min(x_intersect))
+    mid_section2 = NormalDist(mu2, sigma2).cdf(max(x_intersect)) - NormalDist(
+        mu2, sigma2
+    ).cdf(min(x_intersect))
 
     # compute sum of overlap sections based on which middle section is larger
     if mid_section1 < mid_section2:
@@ -251,7 +292,9 @@ def get_union(seg1_df: pd.DataFrame, seg2_df: pd.DataFrame) -> pd.DataFrame:
     return bins
 
 
-def _union_one_sided(seg1_df: pd.DataFrame, seg2_df: pd.DataFrame) -> pd.DataFrame:
+def _union_one_sided(
+    seg1_df: pd.DataFrame, seg2_df: pd.DataFrame
+) -> pd.DataFrame:
     """
     Gets the union of the segment partitions between the two files.
 
@@ -261,22 +304,38 @@ def _union_one_sided(seg1_df: pd.DataFrame, seg2_df: pd.DataFrame) -> pd.DataFra
     """
 
     bin_list = []
-    for chrom in np.arange(1, 23):  # assumes segments in each chromosome (except for XY)
-        segments1 = seg1_df.loc[seg1_df["Chromosome"] == chrom].reset_index(drop=True)
-        segments2 = seg2_df.loc[seg2_df["Chromosome"] == chrom].reset_index(drop=True)
+    for chrom in np.arange(
+        1, 23
+    ):  # assumes segments in each chromosome (except for XY)
+        segments1 = seg1_df.loc[seg1_df["Chromosome"] == chrom].reset_index(
+            drop=True
+        )
+        segments2 = seg2_df.loc[seg2_df["Chromosome"] == chrom].reset_index(
+            drop=True
+        )
 
         pointer2 = 0  # starting at beginning of seg2
-        for i, (start, end) in enumerate(zip(segments1["Start.bp"], segments1["End.bp"])):
+        for i, (start, end) in enumerate(
+            zip(segments1["Start.bp"], segments1["End.bp"])
+        ):
             # create bins for this segment1, updating pointer2 to save computation
             bin_list, pointer2 = create_bins(
-                start, end, segments2, pointer2, segments1.loc[i][STAT_COLUMNS], chrom, bin_list=bin_list
+                start,
+                end,
+                segments2,
+                pointer2,
+                segments1.loc[i][STAT_COLUMNS],
+                chrom,
+                bin_list=bin_list,
             )
 
     bin_df = pd.DataFrame(bin_list)
     return bin_df
 
 
-def create_bins(start, end, segments2, pointer2, segments1_stats, chrom, bin_list=None):
+def create_bins(
+    start, end, segments2, pointer2, segments1_stats, chrom, bin_list=None
+):
     """
     Creates bins over this segment defined by start to end, as compared to segments2 df.
 
@@ -305,27 +364,87 @@ def create_bins(start, end, segments2, pointer2, segments1_stats, chrom, bin_lis
 
     if start < start2:
         if end <= start2:
-            bin_list.append(append_bin(start, end, segments1_stats, None, chrom))  # unique segment
+            bin_list.append(
+                append_bin(start, end, segments1_stats, None, chrom)
+            )  # unique segment
             return bin_list, pointer2
         else:
-            bin_list.append(append_bin(start, start2, segments1_stats, None, chrom))  # unique segment
+            bin_list.append(
+                append_bin(start, start2, segments1_stats, None, chrom)
+            )  # unique segment
 
             if end < end2:
-                bin_list.append(append_bin(start2, end, segments1_stats, segments2.loc[pointer2][STAT_COLUMNS], chrom))
+                bin_list.append(
+                    append_bin(
+                        start2,
+                        end,
+                        segments1_stats,
+                        segments2.loc[pointer2][STAT_COLUMNS],
+                        chrom,
+                    )
+                )
                 return bin_list, pointer2
             else:
-                bin_list.append(append_bin(start2, end2, segments1_stats, segments2.loc[pointer2][STAT_COLUMNS], chrom))
-                return create_bins(end2, end, segments2, pointer2 + 1, segments1_stats, chrom, bin_list=bin_list)
+                bin_list.append(
+                    append_bin(
+                        start2,
+                        end2,
+                        segments1_stats,
+                        segments2.loc[pointer2][STAT_COLUMNS],
+                        chrom,
+                    )
+                )
+                return create_bins(
+                    end2,
+                    end,
+                    segments2,
+                    pointer2 + 1,
+                    segments1_stats,
+                    chrom,
+                    bin_list=bin_list,
+                )
     else:
         if end <= end2:
-            bin_list.append(append_bin(start, end, segments1_stats, segments2.loc[pointer2][STAT_COLUMNS], chrom))
+            bin_list.append(
+                append_bin(
+                    start,
+                    end,
+                    segments1_stats,
+                    segments2.loc[pointer2][STAT_COLUMNS],
+                    chrom,
+                )
+            )
             return bin_list, pointer2
         else:
             if start < end2:
-                bin_list.append(append_bin(start, end2, segments1_stats, segments2.loc[pointer2][STAT_COLUMNS], chrom))
-                return create_bins(end2, end, segments2, pointer2 + 1, segments1_stats, chrom, bin_list=bin_list)
+                bin_list.append(
+                    append_bin(
+                        start,
+                        end2,
+                        segments1_stats,
+                        segments2.loc[pointer2][STAT_COLUMNS],
+                        chrom,
+                    )
+                )
+                return create_bins(
+                    end2,
+                    end,
+                    segments2,
+                    pointer2 + 1,
+                    segments1_stats,
+                    chrom,
+                    bin_list=bin_list,
+                )
             else:
-                return create_bins(start, end, segments2, pointer2 + 1, segments1_stats, chrom, bin_list=bin_list)
+                return create_bins(
+                    start,
+                    end,
+                    segments2,
+                    pointer2 + 1,
+                    segments1_stats,
+                    chrom,
+                    bin_list=bin_list,
+                )
 
 
 def append_bin(start, end, stats1, stats2, chromosome):
@@ -402,9 +521,17 @@ def compute_aad(target: pd.DataFrame, pred: pd.DataFrame) -> float:
     tar_by_chr: defaultdict[int, list[Segment]] = defaultdict(list)
 
     for row in pred.to_numpy():
-        src_by_chr[row[src_chr]].append(Segment(row[src_start], row[src_end], row[src_minor], row[src_major]))
+        src_by_chr[row[src_chr]].append(
+            Segment(
+                row[src_start], row[src_end], row[src_minor], row[src_major]
+            )
+        )
     for row in target.to_numpy():
-        tar_by_chr[row[tar_chr]].append(Segment(row[tar_start], row[tar_end], row[tar_minor], row[tar_major]))
+        tar_by_chr[row[tar_chr]].append(
+            Segment(
+                row[tar_start], row[tar_end], row[tar_minor], row[tar_major]
+            )
+        )
 
     for chr in src_by_chr.keys() & tar_by_chr.keys():
         src_segments = src_by_chr[chr]
@@ -423,7 +550,9 @@ def compute_aad(target: pd.DataFrame, pred: pd.DataFrame) -> float:
                 tar_idx += 1
                 continue
             # There is an intersection!
-            start = max(src_segments[src_idx].start, tar_segments[tar_idx].start)
+            start = max(
+                src_segments[src_idx].start, tar_segments[tar_idx].start
+            )
             end = min(src_segments[src_idx].end, tar_segments[tar_idx].end)
             weights.append((end - start))
             src.append(src_segments[src_idx].minor)
@@ -442,12 +571,16 @@ def compute_aad(target: pd.DataFrame, pred: pd.DataFrame) -> float:
     src = np.array(src)
     tar = np.array(tar)
 
-    params = minimize(lambda p: np.sum(weights * np.abs(src * p[0] + p[1] - tar)), np.array([1, 0])).x
-    return np.sum(weights * np.abs(src * params[0] + params[1] - tar))
+    params = minimize(
+        lambda p: np.sum(weights * np.abs(src * p[0] + p[1] - tar)),
+        np.array([1, 0]),
+    ).x
+    return float(np.sum(weights * np.abs(src * params[0] + params[1] - tar)))
 
 
-# TESTING
-def compare_outputs(src_folder: PathLike, solution_folder: PathLike) -> dict[float, list[float]]:
+def compute_output_aad(
+    src_folder: PathLike, solution_folder: PathLike
+) -> dict[float, list[float]]:
     """
     Computes the AAD of all solved instances.
     """
@@ -455,12 +588,18 @@ def compare_outputs(src_folder: PathLike, solution_folder: PathLike) -> dict[flo
     assert os.path.exists(solution_folder)
     print(src_folder, solution_folder)
 
-    simulated_problems = glob.glob(f"{src_folder}/**/sim_profile.tsv", recursive=True)
+    simulated_problems = glob.glob(
+        f"{src_folder}/**/sim_profile.tsv", recursive=True
+    )
 
-    res = {}
+    res: dict[float, list[float]] = {}
+
+    pur_re = re.compile(r"pur_(\d+)")
 
     for problem_path in simulated_problems:
-        rel_path = "\\".join(problem_path.replace(str(src_folder), "").split("\\")[:-1])
+        rel_path = "\\".join(
+            problem_path.replace(str(src_folder), "").split("\\")[:-1]
+        )
         solution_path = f"{solution_folder}{rel_path}\\ploidities.tsv"
         if not os.path.exists(solution_path):
             continue
@@ -469,9 +608,102 @@ def compare_outputs(src_folder: PathLike, solution_folder: PathLike) -> dict[flo
         pred_df = pd.read_csv(problem_path, sep="\t")
         sol_df = pd.read_csv(solution_path, sep="\t")
 
-        print(compute_aad(sol_df, pred_df))
+        aad = compute_aad(sol_df, pred_df)
+        pur_str = re.findall(pur_re, problem_path)[0]
+        pur = int(pur_str) * 10 ** (1 - len(pur_str))
+
+        res.setdefault(pur, []).append(aad)
+    print(res)
+    return res
+
+
+def compute_output_purity(
+    src_folder: PathLike, solution_folder: PathLike
+) -> dict[float, list[float]]:
+    """
+    Computes the AAD of all solved instances.
+    """
+    assert os.path.exists(src_folder)
+    assert os.path.exists(solution_folder)
+    print(src_folder, solution_folder)
+
+    simulated_problems = glob.glob(
+        f"{src_folder}/**/sim_profile.tsv", recursive=True
+    )
+
+    res: dict[float, list[float]] = {}
+
+    pur_re = re.compile(r"pur_(\d+)")
+
+    for problem_path in simulated_problems:
+        rel_path = "\\".join(
+            problem_path.replace(str(src_folder), "").split("\\")[:-1]
+        )
+        solution_path = f"{solution_folder}{rel_path}\\solutions.tsv"
+        if not os.path.exists(solution_path):
+            continue
+        print(solution_path)
+
+        sol_df = pd.read_csv(solution_path, sep="\t")
+
+        best_sol, best_ll = 0, -np.inf
+
+        for sol, ll in sol_df.to_numpy():
+            if ll > best_ll:
+                (best_sol, best_ll) = sol, ll
+        pur_str = re.findall(pur_re, problem_path)[0]
+        pur = int(pur_str) * 10 ** (1 - len(pur_str))
+
+        res.setdefault(pur, []).append(best_sol)
 
     return res
+
+
+def plot_aad(ax: Axes, aads: dict[float, list[float]]):
+    """
+    Plots the AADs similarly to the HapASeg graph.
+
+    Parameters:
+    * `ax`: The `matplotlib` axis to draw on.
+    * `aads`: A dictionary from the purity to the AADs achieved for it.
+    * `offset`: [Optional]: An offset from the purity. Used to make it easier to compare several AAD results.
+    """
+    xs = []
+    ys = []
+
+    # vxs = []
+    # vys = []
+
+    for pur, vals in aads.items():
+        # vxs.append(pur)
+        # vys.append(list(vals))
+        for val in vals:
+            xs.append(pur)
+            ys.append(val)
+
+    ax.scatter(
+        xs,
+        ys,
+    )
+
+    # ax.violinplot(vys, positions=vxs, widths=1.0 / len(xs), showextrema=False)
+
+    ax.set_xlabel("purity")
+    ax.set_ylabel("AAD")
+
+
+def plot_purity(ax: Axes, purities: dict[float, list[float]]):
+    ax.set_xlabel("Simulated purity")
+    ax.set_ylabel("Simulated purity")
+
+    xs = []
+    ys = []
+    for pur, vals in purities.items():
+        for val in vals:
+            xs.append(pur)
+            ys.append(val)
+
+    ax.scatter(xs, ys)
 
 
 def main():
@@ -490,9 +722,45 @@ def main():
     # print(f"{overlap_length=}")
     # print(f"{bins=}")
 
-    compare_outputs(
-        r"C:\Users\rsolan\Documents\hap-rs\data\mass\ch1022gl", r"C:\Users\rsolan\Documents\hap-rs\out\mass\ch1022gl"
+    _, (ax1, ax2) = plt.subplots(1, 2)  # type: ignore
+
+    ax1.semilogy()
+
+    input_addr = r"C:\Users\rsolan\Documents\hap-rs\data\mass"
+    output_addr = r"C:\Users\rsolan\Documents\hap-rs\out\mass5_1"
+
+    # plot_aad(
+    #     ax1,
+    #     compute_output_aad(
+    #         r"C:\Users\rsolan\Documents\hap-rs\data\mass\ch1022gl",
+    #         r"C:\Users\rsolan\Documents\hap-rs\out\mass4_01",
+    #     ),
+    #     offset=-0.02,
+    # )
+    plot_aad(
+        ax1,
+        compute_output_aad(
+            input_addr,
+            output_addr,
+        ),
     )
+
+    # plot_purity(
+    #     ax2,
+    #     compute_output_purity(
+    #         r"C:\Users\rsolan\Documents\hap-rs\data\mass\ch1022gl",
+    #         r"C:\Users\rsolan\Documents\hap-rs\out\mass4_01",
+    #     ),
+    # )
+    plot_purity(
+        ax2,
+        compute_output_purity(
+            input_addr,
+            output_addr,
+        ),
+    )
+
+    plt.show()
 
 
 if __name__ == "__main__":
